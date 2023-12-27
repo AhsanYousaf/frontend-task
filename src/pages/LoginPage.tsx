e2632314dd789dd086dd9da8b4073893a75ec368
AhsanYousaf <ahsanbhatti624@gmail.com>
2023-12-27 23:17:41 +0500

    added font sora variable

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -11,16 +11,19 @@ const LoginPage = () => {
 
     return (
         <div className="flex flex-col lg:flex-row justify-center items-center h-screen w-full">
-            <div className="w-full lg:w-[40%] h-screen px-16 py-8">
-                <img src={MainIcon} alt="MainIcon" />
-                <p className="lg:text-[64px] lg:font-[600] text-[38px] font-[550] font-sora Variable mt-8">
-                    Welcome back
+            <div className="w-full lg:w-[43%] h-screen px-[74px]">
+                <img className="mt-4" src={MainIcon} alt="MainIcon" />
+                <p style={{ fontFamily: 'SoraVariable' }} className="lg:text-[64px] lg:font-[600] text-[38px] font-[550] mt-4">
+                    Welcome
                 </p>
-                <p className="lg:text-[18px] lg:font-[300] text-[16px] font-[300] font-sans">
+                <p style={{ fontFamily: 'SoraVariable' }} className="lg:text-[64px] lg:font-[600] text-[38px] font-[550] mt-[-16px] lg:mt-[-30px]">
+                    back
+                </p>
+                <p className="lg:text-[18px] lg:font-[300] text-[16px] font-[300]">
                     You need to be signed in to access the project dashboard.
                 </p>
                 <form>
-                    <div className="flex flex-col gap-2 w-full mt-10 lg:text-[16px] lg:font-[500] text-[15px] font-[500]">
+                    <div className="flex flex-col gap-2 w-full mt-6 lg:text-[16px] lg:font-[500] text-[15px] font-[500]">
                         <label>Email or username</label>
                         <input
                             type="text"
@@ -44,7 +47,7 @@ const LoginPage = () => {
                             />
                         </div>
                     </div>
-                    <div className="flex justify-between gap-3 mt-6">
+                    <div className="flex justify-between gap-3 mt-4">
                         <div className="flex justify-center item-center gap-2">
                             <input
                                 type="checkbox"
@@ -60,14 +63,14 @@ const LoginPage = () => {
                             </p>
                         </div>
                     </div>
-                    <div className="mt-6">
+                    <div className="mt-4">
                         <button className='w-full bg-[#50F89A] border border-[#00E687] py-[14px] px-[12px] rounded-sm '>Sign in</button>
                         <div className="w-full flex justify-center border border-[#CFD8E1] py-[14px] px-[12px] mt-6 rounded-sm gap-2 cursor-pointer">
                             <img src={GoogleIcon} alt="GoogleIcon" />
                             <div>Sign in with Google</div>
                         </div>
                     </div>
-                    <div className="flex justify-center mt-6">
+                    <div className="flex justify-center mt-4">
                         <p className="text-[16px] font-[360]">
                             Haven’t joined yet?{" "}
                             <span className="cursor-pointer hover:underline text-[16px] font-[500]">
@@ -77,7 +80,7 @@ const LoginPage = () => {
                     </div>
                 </form>
             </div>
-            <div className="w-full lg:w-[60%] h-screen lg:block hidden">
+            <div className="w-full lg:w-[57%] h-screen lg:block hidden">
                 <img className="w-full h-screen" src={RightSideImage} alt="RightSideImage" />
             </div>
         </div>
