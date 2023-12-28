b8819fe1920e7f0a86497f6efb7b4b66762e584c
AhsanYousaf <ahsanbhatti624@gmail.com>
2023-12-28 11:21:05 +0500

    change logic

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,19 +1,13 @@
 import CommonButton from "../components/CommonButton"
-import { useNavigate } from 'react-router-dom';
 
 const PageNotFound = () => {
-    const navigate=useNavigate();
-    const handleButtonClick = () => {
-        alert("button Clicked")
-      navigate(-1);
-    };
     return (
         <div className="flex items-center justify-center h-screen bg-[#a2d5c6]">
             <div className="text-center">
                 <h1 className="text-6xl font-bold text-gray-800">404</h1>
                 <p className="text-2xl font-semibold text-gray-600">Page Not Found</p>
                 <p className="text-gray-500 mt-4">The page you are looking for might be under construction or does not exist.</p>
-                <CommonButton btnText="Go back" btnClick={handleButtonClick} btnClasses="mt-6 px-6 py-3 rounded-lg bg-[#077b8a] text-white" />
+                <CommonButton btnText="Go back" btnClick={() => window.history.back()} btnClasses="mt-6 px-6 py-3 rounded-lg bg-[#077b8a] text-white" isSubmit={false} />
             </div>
         </div>
     );
